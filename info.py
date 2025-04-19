b8c82a4ad33176418599a5f76feebd30a12468fb
QusaiSalwm <81245445+QusaiSalwm@users.noreply.github.com>
2025-04-19 13:16:19 +0300

    Update info.py

--- info.py
+++ info.py
@@ -67,8 +67,8 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
 DATABASE_URI = environ.get('DATABASE_URI', "")   # IF Multiple Database Is False Then Fill Only This Database Url.
-DATABASE_NAME = environ.get('DATABASE_NAME', "")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', '')
+DATABASE_NAME = environ.get('DATABASE_NAME', "autoFilterBotDB")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'autoFilterBotFiles')
 
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
 O_DB_URI = environ.get('O_DB_URI', "")   # This Db Is For Other Data Store
